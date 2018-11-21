42dc896d4fdfa5c4681cf712f5178200c5366a4d
Pragati Singh <psingh@ENDOINDIA.COM>
2018-11-21 19:36:01 +0530

    -add user to database on signup

--- handler/form_handler.php
+++ handler/form_handler.php
@@ -1,6 +1,15 @@
 <?php
 //start the session
     session_start();
+
+    //connect to database
+    $servername = "localhost";
+    $username = "pragati";
+    $password = "pragati";
+    $database = "myrecords";
+    $conn = new mysqli($servername, $username, $password, $database);
+
+
     //check if login data is submitted
     if(isset($_POST) & !(empty($_POST['login'])))
     {
@@ -16,6 +25,14 @@
     $_SESSION['password'] = $_POST['password'];
     $_SESSION['fullname'] = $_POST['fullname'];
     $_SESSION['contact_no'] = $_POST['contact_no'];
-    header("Location: ../dashboard.php");
+
+    //create a record in database
+    $sql = "INSERT INTO users(fullname, email, contact_no, password)
+            VALUES ('{$_SESSION['fullname']}','{$_SESSION['email']}','{$_SESSION['contact_no']}','{$_SESSION['password']}')";
+
+    if($conn->query($sql))
+        {header("Location: ../dashboard.php");}
+    else
+        {echo "Signup not successfull".$conn->error;}
     }
 ?>
